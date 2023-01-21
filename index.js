169eb3d72511be0954c279b4c32988494be3903b
VladGridin <vldgrdn@gmail.com>
2023-01-21 21:18:47 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const app = express();
-const http = require("http");
+const http = require("https");
 const server = http.createServer(app);
 const { Server } = require("socket.io");
 const io = new Server(server);
@@ -9,7 +9,7 @@ app.get("/", function (request, respons) {
   respons.sendFile(__dirname + "/index.html");
 });
 
-server.listen(3000, () => {
+server.listen("https://testchatvlad.netlify.app/", () => {
   console.log("listening on *:3000");
 });
 
